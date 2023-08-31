8e61b4f1d23f4127fdb95af733e680f430739ca9
Philippos123 <>
2023-08-31 09:23:00 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let button =document.getElementsByTagName("button")
+    let buttons = document.getElementsByTagName("button")
     for(let button of buttons){
         button.addEventListener("click", function(){
             if(this.getAttribute("data-type") === "submit"){
@@ -7,15 +7,26 @@ document.addEventListener("DOMContentLoaded", function(){
             } else{
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
+
 })
 
-function runGame(){
+function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2)
+    } else{
+        alert(`Unknown game type: ${gameType}`);
+        throw`Unknown game type: ${gameType}. Aborting!`;
+
+    }
 }
 function checkAnswer(){
 
@@ -33,7 +44,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent=operand1
+    document.getElementById("operand2").textContent=operand2
+    document.getElementById("operator").textContent="+"
 
 }
 
